d7d45e42d93ecccd0f7b7b0f1ba768c5da417c74
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-25 22:46:18 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- Assignments/M2/CodeCamp_Percolation/Solution.java
+++ Assignments/M2/CodeCamp_Percolation/Solution.java
@@ -4,50 +4,50 @@ import java.util.Scanner;
  */
  class Percolation {
  /**
-  * { integer variable }
+  * { integer variable }.
   */
- 	private int n;
+    private int n;
  /**
-  * { integer variable }
+  * { integer variable }.
   */
- 	private int size;
+    private int size;
  /**
-  * { integer variable }
+  * { integer variable }.
   */
- 	private int first;
+    private int first;
  /**
-  * { integer variable }
+  * { integer variable }.
   */
- 	private int last;
+    private int last;
  /**
-  * { object creation }
+  * { object creation }.
   */
     private WeightedQuickUnionUF wqf;
    /**
-    * { boolean value }
+    * { boolean value }.
     */
     private boolean[] connected;
 /**
- * { integer variable }
+ * { integer variable }.
  */
     private int count;
 /**
  * Constructs the object.
  *
- * @param      N     { parameter_description }
+ * @param      n     { parameter_description }
  */
-    public Percolation(final int n) {
-    	this.n = n;
-    	this.size = n * n;
-    	this.first = size;
-    	this.last = size + 1;
-    	this.count = 0;
-    	connected = new boolean[size];
-    	wqf = new WeightedQuickUnionUF(size + 2);
-    	for (int i = 0; i < n; i++) {
-    		wqf.union(first, i);
-    		wqf.union(last, size - i - 1);
-    	}
+    Percolation(final int n) {
+        this.n = n;
+        this.size = n * n;
+        this.first = size;
+        this.last = size + 1;
+        this.count = 0;
+        connected = new boolean[size];
+        wqf = new WeightedQuickUnionUF(size + 2);
+        for (int i = 0; i < n; i++) {
+            wqf.union(first, i);
+            wqf.union(last, size - i - 1);
+        }
     }
 /**
  * Searches for the first match.
@@ -58,7 +58,7 @@ import java.util.Scanner;
  * @return     { description_of_the_return_value }
  */
     public int indexOf(final int i, final int j) {
-    	return n*(i-1) + (j-1);
+        return n * (i - 1) + (j - 1);
     }
     /**
      * Links open sites.
@@ -67,52 +67,52 @@ import java.util.Scanner;
      * @param      j     { parameter_description }
      */
     public void LinkOpenSites(final int i, final int j) {
-    	if (connected[j] && !wqf.connected(i, j)) {
-    		wqf.union(i,j);
-    	}
+        if (connected[j] && !wqf.connected(i, j)) {
+            wqf.union(i, j);
+        }
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @return     { description_of_the_return_value }
      */
     public int numberOfOpenSites() {
-    	return count;
+        return count;
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @param      i     { parameter_description }
      * @param      j     { parameter_description }
      */
     public void open(final int i, final int j) {
-    	int index = indexOf(i, j);
-    	connected[index] = true;
-    	count++;
-    	int bottom = index + n;
-    	int top = index - n;
-    	if (n == 1) {
-   		wqf.union(first, index);
-   		wqf.union(last, index);
+        int index = indexOf(i, j);
+        connected[index] = true;
+        count++;
+        int bottom = index + n;
+        int top = index - n;
+        if (n == 1) {
+        wqf.union(first, index);
+        wqf.union(last, index);
 
-   	}
-    	if (bottom < size) {
-    		LinkOpenSites(index, bottom);
-    	}
-    	if (top >= 0) {
-    		LinkOpenSites(index, top);
-    	}
-    	if (j == 1) {
-    		if (j !=n) {
-    		LinkOpenSites(index, index + 1);
-    	} return;
-    	}
-    	if (j == n) {
-    		LinkOpenSites(index, index - 1);
-    		return;
-    	}
-    	LinkOpenSites(index, index + 1);
-    	LinkOpenSites(index, index - 1);
+    }
+        if (bottom < size) {
+            LinkOpenSites(index, bottom);
+        }
+        if (top >= 0) {
+            LinkOpenSites(index, top);
+        }
+        if (j == 1) {
+            if (j != n) {
+            LinkOpenSites(index, index + 1);
+        } return;
+        }
+        if (j == n) {
+            LinkOpenSites(index, index - 1);
+            return;
+        }
+        LinkOpenSites(index, index + 1);
+        LinkOpenSites(index, index - 1);
     }  
     /**
      * Determines if open.
@@ -122,38 +122,39 @@ import java.util.Scanner;
      *
      * @return     True if open, False otherwise.
      */
-    public boolean isOpen(final int i, final int j)    // is site (row i, column j) open?
-    {
+    public boolean isOpen(final int i, final int j) {
         return connected[indexOf(i, j)];
     }
     /**
-     * { function_description }
+     * { function_description }.
      *
      * @return     { description_of_the_return_value }
      */
     public boolean percolates() {
-    	return wqf.connected(first, last);
+        return wqf.connected(first, last);
     }
-     
 }
 /**
  * Class for solution.
  */
-public class Solution {
-	private Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(final String[] args) {
-	    Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		Percolation pe = new Percolation(n);
-		// int[][] arr = new int[li][li];
-		while (sc.hasNext()) {
-		String[] tokens = sc.nextLine().split(" ");
-		pe.open(Integer.parseInt(tokens[0]), 
-			Integer.parseInt(tokens[1]));
-		}
-		System.out.println(pe.percolates() 
-			&& pe.numberOfOpenSites() != 0);
-	}
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Percolation pe = new Percolation(n);
+        // int[][] arr = new int[li][li];
+        while (sc.hasNext()) {
+        String[] tokens = sc.nextLine().split(" ");
+        pe.open(Integer.parseInt(tokens[0]),
+            Integer.parseInt(tokens[1]));
+        }
+        System.out.println(pe.percolates() 
+            && pe.numberOfOpenSites() != 0);
+    }
 }
