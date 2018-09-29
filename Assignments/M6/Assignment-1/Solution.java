7642c60158f69c190e341b71b18b091ccf50e0f0
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-29 12:02:17 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- Assignments/M6/Assignment-1/Solution.java
+++ Assignments/M6/Assignment-1/Solution.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 import java.util.*;
 class AddLargeNumbers {
-	//static LinkedList<Integer> num1 = new LinkedList<Integer>();
+	static LinkedList<Integer> num1 = new LinkedList<Integer>();
     
     public static LinkedList numberToDigits(String number) {
-     LinkedList<Integer> num1 = new LinkedList<Integer>();
+     //LinkedList<Integer> num1 = new LinkedList<Integer>();
     	int l1= number.length();
     	for(int i = 0; i < l1; i++) {
     		num1.add(number.charAt(i) - '0');
@@ -16,13 +16,13 @@ class AddLargeNumbers {
     }
 
     public static String digitsToNumber(LinkedList list) {
-    	//int digits = 0;
+    	int digits = 0;
     	String str = "";
-    	/*int l1= num1.size();
+    	int l1= num1.size();
     	for (int i =0; i < l1; i++) {
     	digits =num1.get(i);
     	str += Integer.toString(digits);
-    	 }*/
+    	 }
     	 
     	 return str;
     
