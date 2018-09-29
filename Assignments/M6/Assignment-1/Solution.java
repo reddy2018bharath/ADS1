4eaf288c28d67ca56562a7198d7b82530b69d4f4
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-29 10:48:07 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

---
+++ Assignments/M6/Assignment-1/Solution.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+class AddLargeNumbers {
+	LinkedList<Integer> num1 = new LinkedList<Integer>();
+    
+    public static LinkedList numberToDigits(String number) {
+    	LinkedList<Integer> num1 = new LinkedList<Integer>();
+    	int l1= number.length();
+    	for(int i = 0; i < l1 - 1; i++) {
+    		num1.add(number.charAt(i) - '0');
+    		System.out.println(num1);
+    	}
+    	return null;
+
+    }
+
+    public static String digitsToNumber(LinkedList list) {
+    	return null;
+
+    }
+
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+    	return null;
+
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch(input){
+            case "numberToDigits":
+                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+                break;
+
+            case "addLargeNumbers":
+                pDigits = AddLargeNumbers.numberToDigits(p);
+                qDigits = AddLargeNumbers.numberToDigits(q);
+                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+                System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
+        }
+    }
+    
+}
