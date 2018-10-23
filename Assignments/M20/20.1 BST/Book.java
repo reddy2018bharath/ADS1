2b5eee2a8b48fd6f3d7c49c7bc975db352390ad8
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-10-23 19:50:02 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/M20/20.1 BST/Book.java
+++ Assignments/M20/20.1 BST/Book.java
@@ -1,22 +1,58 @@
+/**
+ * Class for book.
+ */
 class Book implements Comparable<Book> {
-	private String name;
-	private String author;
-	private double price;
-	Book(String n, String a, double p) {
-		name = n;
-		author = a;
-		price = p;
-	}
-	String getName() {
-		return name;
-	}
-	String getAuthor() {
-		return author;
-	}
-	double getPrice() {
-		return price;
-	}
-	public int compareTo(Book b) {
-		return getName().compareTo(b.getName());
-	}
+    /**
+     * name of the book.
+     */
+    private String name;
+    /**
+     * author of the book.
+     */
+    private String author;
+    /**
+     * price of the book.
+     */
+    private double price;
+    /**
+     * Constructs the object.
+     * @param      n     name.
+     * @param      a     author.
+     * @param      p     price.
+     */
+    Book(final String n, final String a,
+        final double p) {
+        name = n;
+        author = a;
+        price = p;
+    }
+    /**
+     * Gets the name.
+     * @return     The name.
+     */
+    String getName() {
+        return name;
+    }
+    /**
+     * Gets the author.
+     * @return     The author.
+     */
+    String getAuthor() {
+        return author;
+    }
+    /**
+     * Gets the price.
+     * @return     The price.
+     */
+    double getPrice() {
+        return price;
+    }
+    /**
+     * compareTo method.
+     * @param      b     b of type Book.
+     * @return     integer.
+     */
+    public int compareTo(final Book b) {
+        return getName().compareTo(b.getName());
+    }
 }
