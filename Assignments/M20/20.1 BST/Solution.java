bbda364577aba9feb6f70133fb3f7df4a4e46365
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-10-23 18:48:33 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/M20/20.1 BST/Solution.java
+++ Assignments/M20/20.1 BST/Solution.java
@@ -4,72 +4,81 @@ import java.util.Scanner;
  */
 public final class Solution {
 
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//Unused.
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BinarySearchTree<Book, Integer> bst = new BinarySearchTree();
-		while (sc.hasNext()) {
-			String[] tokens = sc.nextLine().split(",");
-			switch (tokens[0]) {
-				case "put":
-				Book key = new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1]));
-				int value = Integer.parseInt(tokens[2 + 2]);
-				bst.put(key, value);
-				break;
-				case "get":
-				key = new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1]));
-				System.out.println(bst.get(key));
-				break;
-				case "max":
-				Book b = bst.max();
-				System.out.println(b.getName()+", "+b.getAuthor()+", "+b.getPrice());
-				break;
-				case "min":
-			    b = bst.min();
-				System.out.println(b.getName()+", "+b.getAuthor()+", "+b.getPrice());
-				break;
-				case "select":
-				b = bst.select(Integer.parseInt(tokens[1]));
-                System.out.println(b.getName()+", "+b.getAuthor()+", "+b.getPrice());
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Unused.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree<Book, Integer> bst = new BinarySearchTree();
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(",");
+            switch (tokens[0]) {
+                case "put":
+                Book key = new Book(tokens[1], tokens[2], 
+                    Double.parseDouble(tokens[2 + 1]));
+                int value = Integer.parseInt(tokens[2 + 2]);
+                bst.put(key, value);
                 break;
-				case "floor":
-			    b = bst.floor(new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1])));
-			    if (b == null) {
-			    	System.out.println("null");
-			    	break;
-			    }
-			    System.out.println(b.getName()+", "+b.getAuthor()+", "+b.getPrice());
-				break;
-				case "ceiling":
-				 b = bst.ceiling(new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1])));
-			    if (b == null) {
-			    	System.out.println("null");
-			    	break;
-			    }
-			    System.out.println(b.getName()+", "+b.getAuthor()+", "+b.getPrice());
-				break;
-				case "deleteMax":
-				bst.deleteMax();
-				break;
-				case "deleteMin":
-				bst.deleteMin();
-				break;
-				case "delete":
-				bst.delete(new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1])));
-				break;
-				default:
-				break;
-			}
-		}
-	}
+                case "get":
+                key = new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1]));
+                System.out.println(bst.get(key));
+                break;
+                case "max":
+                Book b = bst.max();
+                System.out.println(
+                    b.getName()+", "+b.getAuthor()+", "+b.getPrice());
+                break;
+                case "min":
+                b = bst.min();
+                System.out.println(
+                    b.getName()+", "+b.getAuthor()+", "+b.getPrice());
+                break;
+                case "select":
+                b = bst.select(Integer.parseInt(tokens[1]));
+                System.out.println(
+                    b.getName()+", "+b.getAuthor()+", "+b.getPrice());
+                break;
+                case "floor":
+                b = bst.floor(
+                    new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1])));
+                if (b == null) {
+                    System.out.println("null");
+                    break;
+                }
+                System.out.println(
+                    b.getName()+", "+b.getAuthor()+", "+b.getPrice());
+                break;
+                case "ceiling":
+                 b = bst.ceiling(
+                    new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1])));
+                if (b == null) {
+                    System.out.println("null");
+                    break;
+                }
+                System.out.println(
+                    b.getName()+", "+b.getAuthor()+", "+b.getPrice());
+                break;
+                case "deleteMax":
+                bst.deleteMax();
+                break;
+                case "deleteMin":
+                bst.deleteMin();
+                break;
+                case "delete":
+                bst.delete(
+                    new Book(tokens[1], tokens[2], Double.parseDouble(tokens[2 + 1])));
+                break;
+                default:
+                break;
+            }
+        }
+    }
 }
