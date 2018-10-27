1c3a08060614c2626cfe39eeb968b7c719062dcd
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-10-27 13:10:55 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/M24/2/Solution.java
+++ Assignments/M24/2/Solution.java
@@ -14,25 +14,25 @@ final class Solution {
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        SeparateChainingHashST<String, Double> hash1 =
+        SeparateChainingHashST<Double, String> hash1 =
         new SeparateChainingHashST();
         /*SeparateChainingHashST<Integer, Double> hash2 =
         new SeparateChainingHashST();*/
         int input = Integer.parseInt(sc.nextLine());
         while (input-- != 0) {
             String[] inp = sc.nextLine().split(",");
-            hash1.put((inp[1]), Double.parseDouble(inp[2]));
+            hash1.put(Double.parseDouble(inp[2]), inp[1]);
             //hash2.put(Integer.parseInt(inp[0]), Double.parseDouble(inp[2]));
         }
         int m = Integer.parseInt(sc.nextLine());
         while (m-- != 0) {
             String[] tokens = sc.nextLine().split(" ");
             if (tokens[0].equals("BE")) {
-                    System.out.println(hash1.get((tokens[1])));
+                    System.out.println(hash1.get(Double.parseDouble(tokens[1])));
             } else if (tokens[0].equals("LE")) {
-                    System.out.println(hash1.get((tokens[1])));
+                    System.out.println(hash1.get(Double.parseDouble(tokens[1])));
             } else if (tokens[0].equals("GE")) {
-                    System.out.println(hash1.get((tokens[1])));
+                    System.out.println(hash1.get(Double.parseDouble(tokens[1])));
             } 
         }
     }
